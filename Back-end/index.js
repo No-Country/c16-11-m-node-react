c2174e9e24866bc90bb16ca93a5756e05742b707
Eduardo Trigo <eduatrigo@gmail.com>
2024-02-13 11:29:17 -0300

    configuracion inical index.js

--- Back-end/index.js
+++ Back-end/index.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const { EXPRESS_CONFIG } = require('./config')
 
 const app = express()
 
 app.use(express.json())
+
+app.listen(EXPRESS_CONFIG.PORT, () => console.log("app listening to port", EXPRESS_CONFIG.PORT))
