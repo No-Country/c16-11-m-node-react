734aed08a5c6dd70d4c52b44c58d05e2016d4663
lexandfr <alexfloresrayme@gmail.com>
2024-02-22 00:50:38 -0500

    actualizar diseño de la página principal y SEO

---
+++ client/src/config/site.ts
@@ -0,0 +1,21 @@
+export const siteConfig = {
+  name: "Hacemos del globo un arte | Detalles para toda ocasión | Arreglos y decoraciones | Detalles Luna",
+  description: "Detalles Luna, Arreglos y decoraciones. Encuentra el detalle perfecto para cada momento en tu vida y hazlo inolvidable. (Avda de Europa 8, Montequinto. 41089 Sevilla)",
+  keywords: "Globos, Helio, Decoraciones, Bautizo, Boda, Comunión, Comuniones, Arcos, Regalos Detalles, Dos Hermanas, Sevilla, Montequinto, Detalles Luna",
+  url: "https://detallesluna.com",
+  image: "/images/logo.jpg",
+
+  // Social media
+  facebook: "https://www.facebook.com/detallesluna",
+  instagram: "https://www.instagram.com/detallesluna",
+  twitter: "https://twitter.com/detallesluna",
+
+  // Contact
+  phone: "9954128609",
+  email: "pedidos@detallesluna.com",
+  address: "Avda de Europa 8, Montequinto. 41089 Sevilla",
+
+  // Open hours
+  openHours: "Lunes a Viernes: 10:00 - 14:00 y 17:30 - 20:30. Sábados: 10:00 - 14:00",
+  openHoursShort: "L-V: 10:00-14:00 y 17:30-20:30. S: 10:00-14:00"
+}
