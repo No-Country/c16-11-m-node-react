77c57ab1a56ae15c0e00cdd8445af39adcb86358
lexandfr <alexfloresrayme@gmail.com>
2024-03-05 00:41:54 -0500

    Actualiza estilos y rutas dinámicas

---
+++ client/src/utils/slider.ts
@@ -0,0 +1,7 @@
+export const defaultSlider = [
+  { img: "/photos/producto2.webp", alt: "First placeholder", active: true },
+  { img: "/photos/producto2.webp", alt: "Second placeholder" },
+  { img: "/photos/producto3.webp", alt: "Third placeholder" },
+  { img: "/photos/producto2.webp", alt: "Second placeholder" },
+  { img: "/photos/producto3.webp", alt: "Third placeholder" },
+];
