497801a5e0c85d938fa11852c063ab806dda589e
Aleja Ramirez <aleja2107@yahoo.com>
2024-03-06 16:52:49 -0500

    Cambios al Home - Actualizcion de imagenes
    Co-authored-by: Alexander Flores Rayme <alexfloresrayme@gmail.com>

--- client/src/utils/slider.ts
+++ client/src/utils/slider.ts
@@ -1,7 +1,7 @@
 export const defaultSlider = [
-  { img: "/photos/producto2.webp", alt: "First placeholder", active: true },
-  { img: "/photos/producto2.webp", alt: "Second placeholder" },
-  { img: "/photos/producto3.webp", alt: "Third placeholder" },
-  { img: "/photos/producto2.webp", alt: "Second placeholder" },
-  { img: "/photos/producto3.webp", alt: "Third placeholder" },
+  { img: "/photos/Empresas/imagen2.jpg", alt: "First placeholder", active: true },
+  { img: "/photos/Empresas/imagen3.jpeg", alt: "Second placeholder" },
+  { img: "/photos/Empresas/imagen4.jpeg", alt: "Third placeholder" },
+  { img: "/photos/Empresas/imagen5.jpeg", alt: "Second placeholder" },
+  { img: "/photos/Empresas/imagen6.jpg", alt: "Third placeholder" },
 ];
